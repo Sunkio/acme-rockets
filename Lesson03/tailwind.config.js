cced51913d79d45095336245efe1899e8af006e4
Tanja Schmidt <taschmi@gmx.net>
2023-03-17 16:51:14 +0100

    add lesson03 (rest of it) - contact form (frontend) and footer

--- Lesson03/tailwind.config.js
+++ Lesson03/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+      }
+    },
   },
   plugins: [],
 }
