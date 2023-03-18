0fc8e73f3ee907521172baca3efdf157e8cbed89
Tanja Schmidt <taschmi@gmx.net>
2023-03-18 08:24:05 +0100

    add 1.version of hamburger menu animation full-screen, header hidden

--- Lesson04/tailwind.config.js
+++ Lesson04/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
       screens: {
